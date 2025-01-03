10939d631af66ebcf6be0587878566485cc40ccf
Rahul <rahulrevanna11@gmail.com>
2025-01-03 16:00:37 +0530

    added cores

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,7 +1,8 @@
 const cors = require('cors');
+console.log(process.env.FRONTEND_URL);
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL || 'https://table-booking-app-3bhw.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
